5ce52e7cec0a1a4b7c2093519aac3156026db93a
Jason Eng <jasoneng10.je@gmail.com>
2024-06-30 21:47:34 +0800

    Add Login Page

---
+++ WebProject/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebProject.Models;
+
+namespace WebProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Do something
+                return RedirectToAction("Index", "Home");
+
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+
+            return View(model);
+        }   
+    }
+}
